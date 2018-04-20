d1251d551b1179f08b0abb1a3db3276d8cd46680
Wayne742 <35707965+Wayne742@users.noreply.github.com>
2018-04-20 21:56:17 +0100

    Imports for libraries and column data.
    Added line to import the numpy library.
    Added line to import the numpy library.
    Added line to read the iris.csv data into the script.
    Added lines to call out the values in each of the four columns of the data.

--- project2018.py
+++ project2018.py
@@ -1,3 +1,12 @@
 # Wayne Reilly 16/04/2018.
 # Programming and Scritping, End of Module Project.
 
+import numpy as np #imports the python module known as numpy.
+
+import matplotlib.pyplot as plt #imports the 
+
+np.genfromtxt('data/iris.csv' , delimiter=',') #reads the iris.csv file into the script.
+firstcol = data[:,0]#calls out the values in the first column of the data.
+seccol = data[:,1]#calls out the values in the second column of the data.
+thircol = data[:,2]#calls out the values in the third column of the data.
+fourcol = data[:,3]#calls out the values in the fourth column of the data.
